19fb26dcb572b13934e518cb5144bf4ff768190c
0xmatriksh <kishmat17@gmail.com>
2024-07-11 22:18:24 +0545

    feat: update header and home page content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,8 +7,33 @@
 </svelte:head>
 
 <section >
-	<div class="w-full bg-gray-700 text-white h-16 text-xl flex items-center pl-28">
+	<div class="w-full bg-[#2E2D3C] text-white h-20 text-3xl flex items-center pl-28">
 		Home
+	</div>
+	<div class="text-xl  py-10 px-20 leading-9">
+		<p> Hello everyone, I am Kishmat Bhattarai. </p>
+
+		<p> I am a software developer with a degree in Computer Science and Information Technology from Lumbini City College. 
+			My professional journey has provided me with diverse experiences in software development, machine learning and artificial 
+			intelligence. I am eager to pursue graduate studies in this domain and aspire to contribute to advancements in AI through research and development. 
+		</p>
+
+        <p>Currently, at Mercantile Cloud, I focus on Document Extraction using Machine Learning and built QnA systems with large language models. 
+			Additionally, I developed cloud services using AWS and Azure, which involved creating Lambda functions and integrating them with other services for an Email Bot Application.
+		   These experiences deepened my expertise in machine learning and cloud computing, enhancing my ability to solve complex problems. 
+		</p>
+
+        <p>At KonnectCraft, I contributed to the enhancement of a Deal Management System built in React and Django and
+		am involved in developing the next version of the front-end using Next.js. My work on integrating deep learning models
+		with applications has solidified my foundation in software development methodologies and AI technologies.
+		</p>
+
+		<p>
+        I am passionate about utilizing machine learning techniques to create practical and impactful solutions. My projects, 
+		such as the Personalized LLM and Post Classification System, reflect my ability to apply machine learning and software 
+		development techniques to real-world applications. With a strong foundation in algorithms, data structures, and core computer 
+		science subjects, I am committed to growing, researching, and building innovative solutions in the AI domain.
+	    </p>
 	</div>
 	
 </section>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -2,29 +2,33 @@
 	import { page } from '$app/stores';
 </script>
 
-<header class="w-full bg-gray-600 text-white py-16 ">
+<header class="w-full bg-[#4A4A5A] text-white py-16 text-lg ">
 
 	<section class="mb-14 w-full flex justify-center">
-		<div class="w-1/2 flex h-72 space-x-6">
+		<div class="w-1/2 flex h-80 space-x-6">
 	
 			<img src="kishmat_bhattarai.jpeg" class="object-contain" alt="kishmat">
 	
 			<div class="flex flex-col h-full w-full space-y-5 ">
-				<h1 class="h-1/6 text-xl">
+				<h1 class="h-1/6 text-3xl text-yellow-200">
 					Kishmat Bhattarai
 				</h1>
 	
-				<div class="h-5/6 flex flex-col p-4 bg-gray-700 space-y-2">
-					<div>Undergraduate Bachelor of Computer Science</div>
-					<div>Software Developer | Python, Web, ML, NLP</div>
+				<div class="h-5/6 flex flex-col p-4 bg-[#2E2D3C] space-y-2">
+					<div class="font-extralight">Undergraduate Bachelor of Computer Science</div>
+					<div class="font-light">
+						Software Developer | Python, Web, ML, NLP
+						<p>Kathmandu, Nepal</p>
+						<p>kishmat17@gmail.com</p>
+					</div>
 				</div>
 			</div>
 	
 		</div>
 	</section>
 
-	<nav class="bg-gray-700 ">
-		<ul class="flex justify-center text-lg space-x-4 h-14 items-center">
+	<nav class="bg-[#2E2D3C] ml-52 ">
+		<ul class="flex justify-center text-2xl space-x-4 h-16 items-center">
 			<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
 				<a href="/">Home</a>
 			</li>
